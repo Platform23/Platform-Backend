f1d262331857777828da194b994fddf888469c38
Platform23 <147644622+Platform23@users.noreply.github.com>
2024-07-08 19:27:09 -0400

    Update cors.ts

--- config/cors.ts
+++ config/cors.ts
@@ -8,7 +8,7 @@ import { defineConfig } from '@adonisjs/cors'
  */
 const corsConfig = defineConfig({
   enabled: true,
-  origin: true,
+  origin: ['https://platformht.com'],
   methods: ['GET', 'HEAD', 'POST', 'PUT', 'DELETE'],
   headers: true,
   exposeHeaders: [],
