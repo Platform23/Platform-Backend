5efafbedafa06efe66128f6e674c7fd3a465dbfd
Platform23 <147644622+Platform23@users.noreply.github.com>
2024-08-27 21:53:01 -0400

    Update user.ts

--- app/models/user.ts
+++ app/models/user.ts
@@ -39,7 +39,7 @@ export default class User extends compose(BaseModel, AuthFinder) {
   @column({ serializeAs: null })
   declare password: string
 
-  @column({ serializeAs: null })
+  @column()
   declare role: number
 
   @column()
